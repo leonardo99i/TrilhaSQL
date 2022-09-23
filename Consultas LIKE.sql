e6598c3c169db7f5bde9ce805edcb70aebccfd4f
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-09-22 23:18:47 -0300

    Mudança de diretorio dos arquivos

--- Consultas LIKE.sql
+++ Consultas LIKE.sql
@@ -1,3 +1,8 @@
+// The first line is selecting all the data from the table. The second line is selecting all the data
+// from the table where the name of the product contains the word "Litros". The third line is selecting
+// all the data from the table where the name of the product starts with the word "Litros". The fourth
+// line is selecting all the data from the table where the name of the product contains the word
+// "Litros" and the flavor is "Laranja".
 SELECT * FROM [TABELA DE PRODUTOS]
 
 SELECT * FROM [TABELA DE PRODUTOS] WHERE [NOME DO PRODUTO] LIKE '%Litros%'
